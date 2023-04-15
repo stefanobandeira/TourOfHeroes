a0ef65ed45685b8e6be8fd7899ed14e572586d62
stefanobandeira <stefanobandeira333@gmail.com>
2023-04-14 23:32:33 -0300

    Adicionando novo component e criando herois

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero.model';
+
+export const HEROES:  Hero[] = [
+
+  {id: 1, name: 'Wolverine'},
+  {id: 2, name: 'Homem Aranha'},
+  {id: 3, name: 'Batman'},
+  {id: 4, name: 'Hulk'},
+  {id: 5, name: 'Homem de Ferro'},
+];
